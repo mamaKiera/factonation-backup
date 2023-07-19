9df7af5fbf1e9dcc45b887178695e969bdf9a0fe
Gear <84141000+macgeargear@users.noreply.github.com>
2023-07-19 11:27:12 +0700

    fix: module card component

--- src/app/module/page.tsx
+++ src/app/module/page.tsx
@@ -26,44 +26,7 @@ const page: FC<pageProps> = async () => {
           </div>
 
           {courses.map((course, i) => {
-            return (
-              <div className="flex flex-col gap-6 mx-auto" key={i}>
-                <div className="flex gap-5 rounded-xl w-[920px] h-[340px] bg-neutral-100 px-6 items-center drop-shadow-md ">
-                  <div>
-                    {/* <Image
-                    src={course.image}
-                    alt="Industry image"
-                    className="flex items-center rounded-xl h-[250px] w-[480px]"
-                  /> */}
-                  </div>
-                  <div className="flex flex-col w-[710px] h-[320px] p-2 gap-3 justify-center">
-                    <div className="flex flex-col gap-2">
-                      <p className="text-xs text-primary-button">
-                        {course.courseName}
-                      </p>
-                      <p className="text-xl font-semibold">
-                        {course.courseName}
-                      </p>
-                      <p className="text-sm text-stone-600">{course.details}</p>
-                      <p className="text-xs font-semibold">
-                        จำนวนวิดีโอ {course.lessons.length} วิดีโอ
-                      </p>
-                    </div>
-                    <div className="flex gap-10 justify-between border-t border-primary-button py-1 ">
-                      <p className="text-xs font-semibold my-2  bg-neutral-100">
-                        {4000}
-                      </p>
-                      <div className="flex items-center gap-0">
-                        <Button className="text-xs font-semibold bg-neutral-100">
-                          ดูรายละเอียดคอร์์ส
-                        </Button>
-                        <ArrowRight size={10} />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
+            return <ModuleCard course={course} key={i} />;
           })}
         </div>
       </div>
