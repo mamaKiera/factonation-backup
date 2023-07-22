95f1c38988d426e7749cf4906e2aa34bfe7eadf9
mamaKiera <ckieranna@gmail.com>
2023-07-22 20:35:57 +0700

    feat: add course UI

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import * as React from "react"
+import * as React from "react";
 import { FC, useEffect, useState } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { useParams } from "next/navigation";
 import useUser from "@/hooks/useUser";
-import { Trash2 } from "lucide-react";
+import { Trash2, PlusCircle } from "lucide-react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -17,65 +17,75 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alertDialog"
+} from "@/components/ui/alertDialog";
+import { Fragment, useRef } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 interface pageProps {
-    params: {
-      id: string;
-    };
-  }
+  params: {
+    id: string;
+  };
+}
 
 const Page: FC<pageProps> = () => {
+  const { id } = useParams();
+  const { user, loading } = useUser(id);
 
-  const { id } = useParams()
-  const { user, loading } = useUser(id)
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
 
-  const [name, setName] = useState(user?.name)
-  const [email, setEmail] = useState(user?.email)
-  const [password, setPassword] = useState(user?.password)
+  const [open, setOpen] = useState(false);
+  const cancelButtonRef = useRef(null);
 
   useEffect(() => {
-    if (user ) {
+    if (user) {
       setName(user.name);
       setEmail(user.email);
-      setPassword(user.password);
     }
   }, [user]);
 
-  const HandleDelete = async (courseId: string, studentId: string) => {
-
+  const handleDelete = async (courseId: string, studentId: string) => {
     //const accessToken = localStorage.getItem('token')
 
     try {
       const response = await fetch(`http://localhost:8000/user/enrollment`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
           // Authorization: `bearer ${accessToken}`,
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({courseId, studentId}),
-      })
-      
-      console.log(response)
+        body: JSON.stringify({ courseId, studentId }),
+      });
+
+      console.log(response);
       if (response.ok) {
         location.reload();
       }
-
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-    
-    return (
+  };
+
+  const handleAddCourse = (e: any) => {
+    e.preventDefault();
+
+    setOpen(true);
+  };
+
+  return (
     <form className="ml-8 mt-16 mr-8">
       <div className="space-y-12 sm:space-y-16">
         <div>
-          <h2 className="text-base font-semibold leading-7 text-gray-900">User</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            User
+          </h2>
 
-          <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
-
-          <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-              <label htmlFor="userid" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+          <div className="mt-10 space-y-8 pb-12 sm:space-y-0 sm:pb-0">
+            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+              <label
+                htmlFor="userid"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
                 User ID (auto-generated)
               </label>
               <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -93,7 +103,10 @@ const Page: FC<pageProps> = () => {
             </div>
 
             <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
                 Name
               </label>
               <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -103,14 +116,19 @@ const Page: FC<pageProps> = () => {
                     id="name"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     value={name}
-                    onChange={(e) => {setName(e.target.value)}}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
                   />
                 </div>
               </div>
             </div>
 
             <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
                 Email
               </label>
               <div className="mt-2 sm:col-span-2 sm:mt-0">
@@ -122,80 +140,172 @@ const Page: FC<pageProps> = () => {
                     autoComplete="email"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     value={email}
-                    onChange={(e) => {setEmail(e.target.value)}}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
                   />
                 </div>
               </div>
             </div>
 
             <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
-                Password
-              </label>
-              <div className="mt-2 sm:col-span-2 sm:mt-0">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    type="text"
-                    name="password"
-                    id="password"
-                    autoComplete="password"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    value={password}
-                    onChange={(e) => {setPassword(e.target.value)}}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-            <label htmlFor="purchased courses" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+              <label
+                htmlFor="purchased courses"
+                className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >
                 Purchased courses
               </label>
-            <ScrollArea className="block w-full max-w-2xl h-72 rounded-md border">
-            <div className="p-4">
-                {user?.enrollment.map((enroll) => (
-                <React.Fragment  key={enroll.courseId}>
-                    <div className="text-sm flex justify-between">
-                      {enroll.courseName} 
-                      <AlertDialog>
-                      <AlertDialogTrigger><Trash2 size={20} color="red" /></AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                          <AlertDialogDescription>
-                            The right to access this course will be removed from this user.
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancel</AlertDialogCancel>
-                          <AlertDialogAction onClick={(e)=> HandleDelete(enroll.courseId, enroll.studentId)}> Yes, delete</AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                    </div>
-                    <Separator className="my-2" />
-                </React.Fragment>
-                ))}
+              <ScrollArea className="block w-full max-w-2xl h-72 rounded-md border">
+                <div className="p-4">
+                  {user?.enrollment.map((enroll) => (
+                    <React.Fragment key={enroll.courseId}>
+                      <div className="text-sm flex justify-between">
+                        {enroll.courseName}
+                        <AlertDialog>
+                          <AlertDialogTrigger>
+                            <Trash2 size={20} color="red" />
+                          </AlertDialogTrigger>
+                          <AlertDialogContent>
+                            <AlertDialogHeader>
+                              <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                              <AlertDialogDescription>
+                                The right to access this course will be removed
+                                from this user.
+                              </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                              <AlertDialogCancel>Cancel</AlertDialogCancel>
+                              <AlertDialogAction
+                                onClick={(e) =>
+                                  handleDelete(
+                                    enroll.courseId,
+                                    enroll.studentId
+                                  )
+                                }
+                              >
+                                {" "}
+                                Yes, delete
+                              </AlertDialogAction>
+                            </AlertDialogFooter>
+                          </AlertDialogContent>
+                        </AlertDialog>
+                      </div>
+                      <Separator className="my-2" />
+                    </React.Fragment>
+                  ))}
+                </div>
+              </ScrollArea>
             </div>
-            </ScrollArea>
+
+            <div className="sm:grid sm:grid-cols-9 sm:items-end sm:gap-4">
+              <button
+                onClick={handleAddCourse}
+                type="submit"
+                className="col-start-6 px-3 py-2 text-xs font-medium text-center inline-flex items-center justify-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Add course &nbsp;
+                <PlusCircle size={20} color="white" />
+              </button>
             </div>
           </div>
         </div>
-        </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className="rounded-md bg-primary-button px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Save
-        </button>
+        {/* modal for course adding */}
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-10"
+            initialFocus={cancelButtonRef}
+            onClose={setOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Add courses for this user
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <div className="p-4">
+                          {user?.enrollment.map((enroll) => (
+                            <React.Fragment key={enroll.courseId}>
+                              <div className="text-sm flex justify-between">
+                                {enroll.courseName}
+                                <PlusCircle size={20} color="green" />
+                              </div>
+                              <Separator className="my-2" />
+                            </React.Fragment>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                        onClick={() => setOpen(false)}
+                      >
+                        Add
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                        onClick={() => setOpen(false)}
+                        ref={cancelButtonRef}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        <div className="border-t mt-8">
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-primary-button px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </div>
       </div>
     </form>
-      
-    );
-  };
-  
-  export default Page;
+  );
+};
+
+export default Page;
