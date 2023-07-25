9bac862d2a8a3d4cd0de465b698f72bebc91b30a
Gear <84141000+macgeargear@users.noreply.github.com>
2023-07-25 22:14:12 +0700

    feat: fix navbar

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,14 +1,5 @@
 import { FC } from "react";
 import { IBM_Plex_Sans_Thai } from "next/font/google";
-import Herosection from "@/components/homePage/Herosection";
-import Problemsection from "@/components/homePage/Problemsection";
-import Benefitsection from "@/components/homePage/Benefitsection";
-import Reviewsection from "@/components/homePage/Reviewsection";
-import Pricesection from "@/components/homePage/Pricesection";
-import Questionsection from "@/components/homePage/Questionsection";
-import Lastsection from "@/components/homePage/Lastsection";
-import Footer from "@/components/homePage/Foooter";
-import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 
 interface layoutProps {
