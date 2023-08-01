d09659566862ef43163fe9183afdd9a8f073ec68
Gear <84141000+macgeargear@users.noreply.github.com>
2023-08-01 17:49:47 +0700

    chore

--- src/lib/payment.ts
+++ src/lib/payment.ts
@@ -1,7 +1,13 @@
 import dotenv from "dotenv";
+import Stripe from "stripe";
 dotenv.config();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripe = new Stripe(stripeSecretKey!, {
+  apiVersion: "2022-11-15",
+});
 
 export const createCheckoutSession = async (priceId: string, id: string) => {
   return await stripe.checkout.sessions.create({
